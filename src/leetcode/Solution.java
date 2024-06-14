9d0dc19a7fb616e6d0bbfb61535e167d4df7720d
Galina316 <galya_sharon@yahoo.com>
2024-06-13 19:18:24 -0500

    solved a stack prob

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -3,9 +3,24 @@ package leetcode;
 import java.util.Stack;
 
 public class Solution {
+    //if opening then put into stack
+    //else get the el from stack and check types
+       // if types match then continue
+       // else return false
+
     public boolean isValid(String s) {
-        Stack<Character> c = new Stack<>();
-        return false;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i<s.length();i++) {
+            char par = s.charAt(i);
+            if(par=='(' || par=='[' || par=='{')stack.push(par);
+            else {
+                if(stack.isEmpty())return false;
+               char el = stack.pop();
+               if(par ==')'&& el!='('|| par==']'&&el!='['||par=='}'&& el !='{' ) return false;
+            }
+        }
+
+        return stack.isEmpty();
 
     }
 }
